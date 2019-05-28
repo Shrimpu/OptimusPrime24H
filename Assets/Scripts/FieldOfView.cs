9c4b41e832881cf5b901a036902ec44c8b297a96
Shrimpu <joar.andersson@elev.ga.lbs.se>
2019-05-28 10:08:48 +0200

    Guard semi-done

---
+++ Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,199 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public delegate void CanSeeTarget(Transform target);
+    public CanSeeTarget canSeeTarget;
+
+    public float viewRadius = 5f;
+    [Range(0, 360)]
+    public float viewAngle = 40f;
+    [Space]
+    public float meshResolution = 3f;
+    public int edgeResolveIterations = 4;
+    public MeshFilter viewMeshFilter;
+    public float edgeDistanceTreshold = 0.5f;
+    [Space]
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+
+    Mesh viewMesh;
+
+    private void Start()
+    {
+        viewMesh = new Mesh();
+        viewMesh.name = "View Mesh";
+        viewMeshFilter.mesh = viewMesh;
+
+        StartCoroutine(FindTargetWithDelay(.25f));
+    }
+
+    private void LateUpdate()
+    {
+        DrawFieldOfView();
+    }
+
+    IEnumerator FindTargetWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTarget();
+        }
+    }
+
+    void FindVisibleTarget()
+    {
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+        if (targetsInViewRadius.Length > 0)
+        {
+            Transform target = targetsInViewRadius[0].transform; // there can only be one player
+            Vector3 directionToTarget = target.position - transform.position;
+            if (Vector3.Angle(transform.forward, directionToTarget) < viewAngle / 2)
+            {
+                float distanceToTarget = directionToTarget.magnitude;
+                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstacleMask))
+                {
+                    canSeeTarget?.Invoke(target);
+                }
+            }
+        }
+    }
+
+    void DrawFieldOfView()
+    {
+        int stepCount = Mathf.RoundToInt(viewAngle * meshResolution);
+        float stepAngleSize = viewAngle / stepCount;
+        List<Vector3> viewPoints = new List<Vector3>();
+        ViewCastInfo oldViewCastInfo = new ViewCastInfo();
+
+        for (int i = 0; i <= stepCount; i++)
+        {
+            float angle = transform.eulerAngles.y - viewAngle / 2 + stepAngleSize * i;
+            ViewCastInfo newViewCast = ViewCast(angle);
+
+            if (i > 0)
+            {
+                bool edgeDistanceTresholdExceeded = Mathf.Abs(oldViewCastInfo.distance - newViewCast.distance) > edgeDistanceTreshold;
+                if (oldViewCastInfo.hit != newViewCast.hit || oldViewCastInfo.hit && newViewCast.hit && edgeDistanceTresholdExceeded)
+                {
+                    EdgeInfo edge = FindEdge(oldViewCastInfo, newViewCast);
+                    if (edge.pointA != Vector3.zero)
+                    {
+                        viewPoints.Add(edge.pointA);
+                    }
+                    if (edge.pointB != Vector3.zero)
+                    {
+                        viewPoints.Add(edge.pointB);
+                    }
+                }
+            }
+
+            viewPoints.Add(newViewCast.point);
+            oldViewCastInfo = newViewCast;
+        }
+
+        int vertexCount = viewPoints.Count + 1;
+        Vector3[] vertices = new Vector3[vertexCount];
+        int[] triangles = new int[(vertexCount - 2) * 3];
+
+        vertices[0] = Vector3.zero;
+        for (int i = 0; i < vertexCount - 1; i++)
+        {
+            vertices[i + 1] = transform.InverseTransformPoint(viewPoints[i]);
+
+            if (i < vertexCount - 2)
+            {
+                triangles[i * 3] = 0;
+                triangles[i * 3 + 1] = i + 1;
+                triangles[i * 3 + 2] = i + 2;
+            }
+        }
+
+        viewMesh.Clear();
+        viewMesh.vertices = vertices;
+        viewMesh.triangles = triangles;
+        viewMesh.RecalculateNormals();
+
+    }
+
+    EdgeInfo FindEdge(ViewCastInfo minViewCast, ViewCastInfo maxViewCast)
+    {
+        float minAngle = minViewCast.angle;
+        float maxAngle = maxViewCast.angle;
+        Vector3 minPoint = Vector2.zero;
+        Vector3 maxPoint = Vector2.zero;
+
+        for (int i = 0; i < edgeResolveIterations; i++)
+        {
+            float angle = (minAngle + maxAngle) / 2;
+            ViewCastInfo newViewCast = ViewCast(angle);
+
+            bool edgeDistanceTresholdExceeded = Mathf.Abs(minViewCast.distance - newViewCast.distance) > edgeDistanceTreshold;
+            if (newViewCast.hit == minViewCast.hit && !edgeDistanceTresholdExceeded)
+            {
+                minAngle = angle;
+                minPoint = newViewCast.point;
+            }
+            else
+            {
+                maxAngle = angle;
+                maxPoint = newViewCast.point;
+            }
+        }
+        return new EdgeInfo(minPoint, maxPoint);
+    }
+
+    ViewCastInfo ViewCast(float globalAngle)
+    {
+        Vector3 direction = DirectionFromAngle(globalAngle, true);
+        if (Physics.Raycast(transform.position, direction, out RaycastHit hit, viewRadius, obstacleMask))
+        {
+            return new ViewCastInfo(true, hit.point, hit.distance, globalAngle);
+        }
+        else
+        {
+            return new ViewCastInfo(false, transform.position + direction * viewRadius, viewRadius, globalAngle);
+        }
+    }
+
+    public Vector3 DirectionFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+
+    public struct ViewCastInfo
+    {
+        public bool hit;
+        public Vector3 point;
+        public float distance;
+        public float angle;
+
+        public ViewCastInfo(bool hit, Vector3 point, float distance, float angle)
+        {
+            this.hit = hit;
+            this.point = point;
+            this.distance = distance;
+            this.angle = angle;
+        }
+    }
+
+    public struct EdgeInfo
+    {
+        public Vector3 pointA;
+        public Vector3 pointB;
+
+        public EdgeInfo(Vector3 pointA, Vector3 pointB)
+        {
+            this.pointA = pointA;
+            this.pointB = pointB;
+        }
+    }
+}
--- Assets/Scripts/GuardPatrol.cs
+++ Assets/Scripts/GuardPatrol.cs
@@ -1,14 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(FieldOfView))]
 public class GuardPatrol : MonoBehaviour
 {
+    public delegate void PlayerDetected(GameObject GuardOfTheMonth);
+    public PlayerDetected playerDetected;
+
+    [Header("General stats")]
     public bool loop;
     public float moveSpeed;
     public float turnSpeed;
+    [Header("Detection And Chase")]
+    public float timeSuspicious = 0.5f;
+    public float watchTimeTillDetected = 2f;
+    public float speedOfDetectionLevelDecrease = 0.4f;
+    public float viewRadiusOnChase = 12f;
+    public float minDistanceFromTargetOnChase = 1.5f;
+    public Slider detectionSlider;
 
+    int checkPointIndex = 0;
+    bool chasing = false;
+    [Space]
     public Checkpoint[] checkpoints;
+
+    FieldOfView fow;
+
+    Coroutine moveCoroutine;
 
     [System.Serializable]
     public class Checkpoint
@@ -19,7 +39,23 @@ public class GuardPatrol : MonoBehaviour
 
     private void Start()
     {
-        StartCoroutine(Move());
+        fow = GetComponent<FieldOfView>();
+        fow.canSeeTarget += (target) =>
+        {
+            if (!chasing)
+                StartCoroutine(ChaseTarget(target));
+        };
+        StartPatrolling();
+    }
+
+    public void StopPatrolling()
+    {
+        StopCoroutine(moveCoroutine);
+    }
+
+    public void StartPatrolling()
+    {
+        moveCoroutine = StartCoroutine(Move(checkpoints[0]));
     }
 
     Checkpoint GetNextPoint(int currentPoint)
@@ -36,30 +72,108 @@ public class GuardPatrol : MonoBehaviour
         return checkpoints[nextPoint];
     }
 
-    IEnumerator Move()
+    IEnumerator Move(Checkpoint startPoint)
     {
-        int i = 0;
-        Checkpoint currentPoint = checkpoints[0];
+        Checkpoint currentPoint = startPoint;
 
         while (true)
         {
+            float framesPast = 0;
+            while (true)
+            {
+                yield return null;
+                bool doneRotating = LookAtTarget(currentPoint.checkpointPosition.position);
+                if (doneRotating)
+                    break;
+                framesPast++;
+                if (framesPast + 10 > turnSpeed / Time.deltaTime) // Safety exit
+                    break;
+            }
+
             while (transform.position != currentPoint.checkpointPosition.position)
             {
-                LookAtTarget(currentPoint.checkpointPosition.position);
                 transform.position = Vector3.MoveTowards(transform.position, currentPoint.checkpointPosition.position, moveSpeed * Time.deltaTime);
                 yield return null;
             }
 
             yield return new WaitForSeconds(currentPoint.timeAtCheckpoint);
-            currentPoint = GetNextPoint(i);
-            i++;
+            currentPoint = GetNextPoint(checkPointIndex);
+            checkPointIndex++;
         }
     }
 
-    void LookAtTarget(Vector3 target)
+    IEnumerator ChaseTarget(Transform target)
     {
-        Vector3 targetRotation = target - transform.position;
+        chasing = true;
+        StopPatrolling();
+        float timeTargetHasBeenInvisible = 0;
+        float timeTargetHasBeenVisible = 0;
+        while (true)
+        {
+            yield return null;
+            DisplayDetectionPercent(timeTargetHasBeenVisible);
 
-        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.FromToRotation(transform.forward, targetRotation), turnSpeed * Time.deltaTime);
+            Vector3 directionToTarget = target.position - transform.position;
+            float distanceToTarget = directionToTarget.magnitude;
+            if (distanceToTarget < viewRadiusOnChase && !Physics.Raycast(transform.position, directionToTarget, distanceToTarget, fow.obstacleMask))
+            {
+                timeTargetHasBeenInvisible = 0;
+                LookAtTarget(target.position);
+                transform.position = Vector3.MoveTowards(transform.position, target.position - directionToTarget.normalized * minDistanceFromTargetOnChase, moveSpeed * Time.deltaTime);
+                timeTargetHasBeenVisible += Time.deltaTime;
+                if (timeTargetHasBeenVisible >= watchTimeTillDetected)
+                {
+                    timeTargetHasBeenVisible = watchTimeTillDetected;
+                    chasing = false;
+                    playerDetected?.Invoke(gameObject);
+                    break;
+                }
+            }
+            else
+            {
+                timeTargetHasBeenVisible -= Time.deltaTime * speedOfDetectionLevelDecrease;
+                if (timeTargetHasBeenVisible <= 0)
+                {
+                    timeTargetHasBeenVisible = 0;
+                    timeTargetHasBeenInvisible += Time.deltaTime;
+                    if (timeTargetHasBeenInvisible >= timeSuspicious)
+                    {
+                        chasing = false;
+                        moveCoroutine = StartCoroutine(Move(GetNextPoint(checkPointIndex - 2)));
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    bool LookAtTarget(Vector3 target)
+    {
+        Vector3 targetDir = target - transform.position;
+
+        float step = turnSpeed * Time.deltaTime;
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0f);
+        transform.rotation = Quaternion.LookRotation(newDir);
+        if (newDir.normalized == targetDir.normalized || targetDir == Vector3.zero)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    void DisplayDetectionPercent(float TimeDetected)
+    {
+        float detectionPercent = TimeDetected / watchTimeTillDetected;
+        if (detectionPercent > 0.05f)
+        {
+            detectionSlider.value = detectionPercent;
+            detectionSlider.enabled = true;
+        }
+        else
+        {
+            detectionSlider.value = detectionPercent;
+            detectionSlider.enabled = false;
+        }
     }
 }
